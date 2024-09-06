6207e0125064ca777874cdc3e1430bd7c0768163
sazzadhossainapple <sazzadappple81@gmail.com>
2024-09-06 21:38:20 +0600

    finanac chart addeda

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
+import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard';
 import React from 'react';
 
@@ -26,7 +27,9 @@ const AdminPage = () => {
                     </div>
                 </div>
                 {/* bottom chart */}
-                <div className="w-full h-[500px]"></div>
+                <div className="w-full h-[500px]">
+                    <FinanceChart />
+                </div>
             </div>
             {/* right */}
             <div className="w-full lg:w-1/3">r</div>
